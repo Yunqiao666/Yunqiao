4cf4d43d3beb9420da0f26c838c5aa1a1c0f3484
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-27 16:04:05 +0800

    Add files via upload

---
+++ test_1_4.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+float ggg = 2021;
+extern int gggg;
+int main()
+{
+	printf("崔云乔\n");
+	{int a = 10;
+	printf("a=%d\n", a);
+	printf("gggg=%d\n", gggg); }
+	printf("ggg=%f\n", ggg);
+	return 0;
+}
