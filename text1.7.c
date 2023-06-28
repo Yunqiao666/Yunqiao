a6a128bf4ff2be94740d73d801abfdf08eb3151b
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-28 08:16:13 +0800

    Add files via upload

---
+++ text1.7.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	printf("c;\\text\\text.c");
+//	printf("c;\n");
+//	printf("(auou???)");
+//	printf("%c\n", '\'');
+//	printf("\a\a\a");
+//	printf("%c\n", '\101');
+//	printf("%c\n", '\x30');
+//
+//	return 0;
+//}
