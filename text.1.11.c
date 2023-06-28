3af630524ffbdd08e1d82db6e5d336235f3046e2
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-28 13:36:36 +0800

    Add files via upload

--- text.1.11.c
+++ text.1.11.c
@@ -1,46 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//int main()
-//{
-//	//int a = 9 / 3;
-//	//float a = 9.0/2.0;
-//	int a = 9 % 2;//取余数
-//	printf("%d\n", a);
-//	return 0;
-//}
-//int main()
-//{
-//	int a = 2;
-//	int b= a << 1;
-//	printf("%d\n", b);
-//	return 0;
-//}
-//int main()
-//{
-//	int a = 0;
-//	a = a + 5;
-//	a += 5;
-//
-//	a = a % 3;
-//	a %= 3;
-//	printf(" %d\n", o);
-//	return 0;
-//
-//}
-//int main()
-//{
-//	int a = 10;
-//	printf("%d\n", sizeof(a));
-//	return 0;
-//}
-//int main()
-//{
-//	int arr[10] = { 0 };
-//	printf("%d\n", sizeof(arr[0]));
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	printf("%d\n", sz);
-//	return 0;
-//}
+int main()
+{
+	//int a = 9 / 3;
+	//float a = 9.0/2.0;
+	int a = 9 % 2;//取余数
+	printf("%d\n", a);
+	return 0;
+}
+int main()
+{
+	int a = 2;
+	int b= a << 1;
+	printf("%d\n", b);
+	return 0;
+}
+int main()
+{
+	int a = 0;
+	a = a + 5;
+	a += 5;
+
+	a = a % 3;
+	a %= 3;
+	printf(" %d\n", o);
+	return 0;
+
+}
+int main()
+{
+	int a = 10;
+	printf("%d\n", sizeof(a));
+	return 0;
+}
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%d\n", sizeof(arr[0]));
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", sz);
+	return 0;
+}
 
 
 
