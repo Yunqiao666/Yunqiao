bb2399823a5767425506585b8b1c7688340f3f43
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-28 17:34:27 +0800

    Add files via upload

---
+++ text.1.12.00.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	register int num = 100;//建议num的值存放在寄存器中
+//	return 0;
+//}
+////typedef类型重定义
+//typedef unsigned int u;
+//int main()
+//{   unsigned int num = 100;
+//	u nux = 100;
+//	return 0;
+//}
+////static 静态的
+////修饰局部变量
+//void text()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d\n", a);
+//
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		text();
+//		i++;
+//	}
+//	return 0;
+//}
+//修饰全局变量
+extern int g_val;
+int main()
+{
+	printf("%d\n,", g_val);
+	return 0;
+}
