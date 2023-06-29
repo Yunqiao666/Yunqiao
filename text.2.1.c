685f7c56b128c90b4bf24ab27ef3400b805b6e63
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-29 13:49:59 +0800

    Add files via upload

---
+++ text.2.1.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//打印小飞机
+//int main()
+//{
+//	printf("     **\n");
+//	printf("     **\n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *\n");
+//	printf("    *  *\n");
+//
+//	return 0;
+//}
+////求一个整数是否能被5整除
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//
+//	if (m % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+//求一个数是否能被9整除
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	if (m % 9 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+//
+//
+////输入两个数并写出其较大值
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//	    return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//    int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int M=Max(a,b);
+//	printf("%d\n", M);
+//	return 0;
+//}
+////输入两个数比较出其最小值
+//int Max(x,y)
+//{
+//	if (x < y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a;
+//	int b;
+//	scanf("%d %d", &a, &b);
+//	int m=Max(a,b);
+//	printf("%d\n", m);
+//	return 0;
+//}
