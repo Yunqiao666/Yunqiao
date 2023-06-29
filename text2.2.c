34e87ba2ae96f562d5dba8f5db3e4246e55560c3
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-29 22:57:35 +0800

    Add files via upload

---
+++ text2.2.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	while (1)
+//		printf("haha");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i < 10)
+//	{
+//		if (i % 2 == 0);
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i  == 6);
+//		    break;
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf(" %d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ch=getchar();
+////	printf("%c\n", ch);
+//	putchar(ch);
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//	printf("请确认密码(Y/N):>");
+//	int tmp = 0;
+//	while((tmp=getchar()) != '\n')
+//	{
+//		;
+//    }
+//	int ch = getchar();
+//	if (ch == 'Y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("确认失败\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch=getchar())!=EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+int main()
+{
+	int i = 1;
+	while (i < 10)
+	{
+		printf(" %d", i);
+		i++;
+	}
+	return 0;
+}
