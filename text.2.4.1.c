7eff78824d147e845c0bbca8f31000a10450ec84
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-30 23:12:19 +0800

    Add files via upload

---
+++ text.2.4.1.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//#include<windows.h>
+//int main()
+//{
+//	char a1[] = "uytre hfkg tytkgfsefjsz7654";
+//	char a2[] = "###########################";
+//	int t = 0;
+//	int w = sizeof(a1) - 1;
+//	char c;
+//	while (t <= w)
+//	{
+//		a2[t] = a1[t];
+//		a2[w] = a1[w];
+//			printf("%s\n", a2);
+//			Sleep(1000);
+//			system("cls");
+//		t++;
+//		w--;
+//	}
+//	printf("%s\n", a2);
+//
+//	return 0;
+//}
