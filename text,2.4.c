7d03c1cfc11139276d54ff9f7c79f84bb55f933e
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-30 14:42:35 +0800

    Add files via upload

---
+++ text,2.4.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} while ( i <=10);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if(i==5)
+//		     break;
+//      printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
