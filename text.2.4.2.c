eee8092245cd6ebfa6ee7e8e2eb3a4cea663255f
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-06-30 23:12:37 +0800

    Add files via upload

---
+++ text.2.4.2.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char mm[20] = {0};
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s", &mm);
+//		if (strcmp(mm, "123455") == 0)
+//		{
+//			printf("登录成功\n");
+//			break;
+//		}
+//		else
+//			printf("密码错误请重试\n");
+//	}
+//	printf("三次输入错误，推出程序\n");
+//
+//	return 0;
+//}
