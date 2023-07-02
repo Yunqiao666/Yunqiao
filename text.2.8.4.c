4428536248f9d35673ae1f307e697d1d0312a1c2
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-02 20:13:44 +0800

    Add files via upload

--- text.2.8.4.c
+++ text.2.8.4.c
@@ -1,38 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-////输入1729打印出其各位数之和
-//int accumulate(i)
-//{
-//	if (i > 9)
-//	{
-//		return accumulate(i / 10) + i % 10;
-//	}
-//	else
-//	{
-//		return i;
-//	}
-//}
-//int main()
-//{
-//	int i = 1729;
-//	printf("%d\n", accumulate(i));
-//	return 0;
-//}
-//实现n的k次方
-double power(n, k)
+#include <stdio.h>
+
+double power(double n, int k)
 {
-	if (k > 0)
-		return power(n, k - 1) * n;
-	else if (k == 0)
-		return 1.0;
-	else 
-		return 1,0 / (power(n, -k));
+    if (k > 0)
+        return power(n, k - 1) * n;
+    else if (k == 0)
+        return 1.0;
+    else
+        return 1.0 / power(n, -k);
 }
+
 int main()
 {
-	int n = 0;
-	int k = 0;
-	scanf("%d %d",&n,&k);
-	printf("%d\lf",power(n,k));
-	return 0;
+    double n = 0.0; // 将 n 的类型更改为 double
+    int k = 0;
+    scanf("%lf %d", &n, &k); // 将 n 的输入格式符修改为 %lf
+    printf("%lf\n", power(n, k)); // 将输出格式符修改为 %lf
+    return 0;
 }
