d35ff2d615050b8bab97d70e2b4e1dba98fee648
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-03 12:22:41 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#define ROW 10
+#define COL 10
+
+void initialize(char board[ROW][COL], int row, int col);
+void printfboard(char board[ROW][COL], int row, int col);
