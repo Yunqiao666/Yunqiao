30d883dbb94cbb2a96ed501c18692a06782de57a
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-03 17:03:03 +0800

    Add files via upload

--- game.h
+++ game.h
@@ -2,8 +2,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<windows.h>
-#define ROW 10
-#define COL 10
-
+#include<time.h>
+#define ROW 3
+#define COL 3
+//初始化棋盘为空格
 void initialize(char board[ROW][COL], int row, int col);
+//打印棋盘
 void printfboard(char board[ROW][COL], int row, int col);
+//玩家走
+void plearmove(char board[ROW][COL],int row,int col);
+//电脑走
+void computermove(char board[ROW][COL], int row, int col);
+//判断状态
+char InWin(char board[ROW][COL], int row, int col);
+//判断是否平局
+int IsFull(char board[ROW][COL], int row, int col);
+
+
