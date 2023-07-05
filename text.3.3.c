1aa0b3cfb04712542faa018f5a1543f8f6251a9e
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-05 21:09:59 +0800

    Add files via upload

---
+++ text.3.3.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void text()
+//{
+//}
+//int main()
+//{
+//	int a1[10] = { 0 };
+//	int sz = sizeof(a1) / sizeof(a1[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		a1[i] = i + 1;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		text();
+//		printf("%d\n", a1[i]);
+//	}
+//	return 0;
+//}
