e32e984cf85c2c3be704cba8387755d98c45a658
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-06 21:34:23 +0800

    Add files via upload

---
+++ text.3.3.2.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//void my_strcpy(char* ar1, char *ar2)
+//{
+//	while (*ar1++ = *ar2++){ }
+//}
+//
+////Ä£Äâ
+////void my_strcpy(char* ar1,char* ar2)
+////{
+////	while (*ar2 != '\0')
+////	{
+////		*ar1 = *ar2;
+////		ar1++;
+////		ar2++;
+////	}
+////	*ar1 = *ar2;
+////}
+//int main()
+//{
+//	char arr1[20] = "xxxxxxxxxxxx";
+//	char arr2[] = { "hello" };
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 100;
+//	int o = 0;
+//	for (o =1; o <=i; o++)
+//	{
+//		if (o % 2 !=0 )
+//		{
+//			printf("%d ", o);
+//		}
+//	}
+//	return 0;
+//
+//int my_strlen(char* a1)
+//{
+//
+//	int i = 0;
+//	int m = 0;
+//		while (*a1++ != '\0')
+//			i++;
+//		return i;
+//}
+//
+//int main()
+//{
+//	char a1[] = { "dsgfghdf" };
+//	printf("%d ", my_strlen(a1));
+//	return 0;
+//}
