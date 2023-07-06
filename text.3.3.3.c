780369a45e26f882e8fd7c9511e9dec428e2f644
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-06 21:34:39 +0800

    Add files via upload

---
+++ text.3.3.3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include <assert.h>
+//int my_strlen(const char* a1)
+//{
+//	assert(a1);
+//	int i = 0;
+//	while (*a1 != '\o')
+//	{
+//		a1++;
+//		i++;
+//	}
+//	return i;
+//}
+//int mian()
+//{
+//	char a1[] = {"dsfjkj"};
+//	printf("%d", my_strlen(a1));
+//	return 0;
+//}
