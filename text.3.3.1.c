503a4c8052e6b012527a45e669d0b5a7c89a9dff
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-06 21:33:57 +0800

    Add files via upload

---
+++ text.3.3.1.c
@@ -0,0 +1,40 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void text2()
+//{
+//	printf("ºÇºÇ");
+//}
+//void text1()
+//{
+//	text2();
+//}
+//void text()
+//{
+//	text1();
+//}
+//int main()
+//{
+//	text();
+//	return 0;
+//}
+////¼ÆËã1£¡ + 2£¡ + ... + 10£¡
+//int main()
+//{
+//	//nµÄ½×³Ë
+//	int n = 0;
+//	int i = 0;
+//	int he = 1;
+//	int j = 0;
+//	int sz = 0;
+//	scanf("%d", &n);
+//	for (j = 1; j <= n; j++)
+//	{
+//		for (he=1,i = 1; i <= j; i++)
+//		{
+//			he *= i;
+//		}
+//		sz += he;
+//	}
+//	printf("%d",sz);
+//	return 0;
+//}
