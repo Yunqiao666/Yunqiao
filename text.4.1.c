df3aac6657f266ab03d72dc49a91c790b2f8e84f
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-07 21:52:05 +0800

    Add files via upload

---
+++ text.4.1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<>
+//int main()
+//
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n",*pFloat);
+//
+//	return 0;
+//}
+int main()
+{
+	float f = 5.5f;
+	//101.1
+	//1.011*2^2
+	//s=0  m=1.011  e=2
+	//s=0  m=011   e=2+127
+	//0100 0000 1011 0000 0000 0000 0000 0000
+	//40 b0 00 00
+	return 0;
+}
