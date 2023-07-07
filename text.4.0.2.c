3679cf32ec3b1f56cce3dbdaafbc2ad55432fea1
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-07 21:50:59 +0800

    Add files via upload

---
+++ text.4.0.2.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//	//∫„≥…¡¢
+//}
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));
+//	return 0;
+//}
+unsigned char i = 0;
+int main()
+{
+	for (i = 0; i <= 255; i++)
+	{
+		printf("heheheheh");
+	}
+	return 0;
+	//À¿—≠ª∑
+}
