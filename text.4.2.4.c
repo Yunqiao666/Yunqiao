83d5f31438b8dee7d4c344d4a15f1e19959915e1
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-09 00:07:00 +0800

    Add files via upload

---
+++ text.4.2.4.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int a;
+//int num;
+//int mai(n)
+//{
+//	for (int i = 1, a = 0; i <= n; i++)
+//	{
+//		if (n == 1)
+//			return 0;
+//		if (i == n)
+//		{
+//			if (i % 2 == 0)
+//			{
+//				a++;
+//				num += a;
+//				if (a == 1)
+//				{
+//					break;
+//				}
+//
+//				mai(a);
+//			}
+//			else
+//			{
+//				num += a;
+//				a++;
+//				mai(a);
+//			}
+//		}
+//		else if (i % 2 == 0)
+//		{
+//			a++;
+//		}
+//
+//	}
+//	return num;
+//}
+//int main()
+//{
+//	int n = 10;
+//	printf("%d ", mai(n));
+//	return 0;
+//}
