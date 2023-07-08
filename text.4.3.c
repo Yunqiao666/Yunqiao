72d42a78aeb7dfbc93a7f33c5f5efb6f14401466
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-09 00:09:50 +0800

    Add files via upload

---
+++ text.4.3.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void print(int* ptr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz, i++)
+//	{
+//		printf("%d", *(ptr + i));
+//	}
+//}
+//void test(char* b)
+//{
+//
+//}
+//int main()
+//{
+//	int arr[10] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//p是一级指针
+//	print(p, sz);
+//	char ch = 'w';
+//	char* p1 = &ch;
+//	test(&ch);//char*
+//	test(p1);
+//	return 0;
+//}
+//text(int** pa)
+//{
+//	**pa = 20;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;//一级指针
+//	int** ppa = &pa;//二级指针
+//	text(ppa);
+//	text(&pa);
+//	int* arr[20] = { 0 };
+//	text(arr);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int var(int x,int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//
+//	char ch = 'w';
+//	char* pch = &ch;
+//
+//	int a1[10] = { 0 };
+//	int(*pa1)[10] = &a1;
+////	数组名 != &数组名;
+////	函数名 == &函数名;
+//	printf("%d\n", var);
+//	printf("%d\n", &var);
+//	int (*pf)(int, int) = var;
+//    int ret = (*pf)(5, 6);
+//	printf("%d", ret);
+//}
+
