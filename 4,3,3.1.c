c4c19e926ac67aedea107717d0a38cdf1291372b
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-09 00:12:30 +0800

    Add files via upload

---
+++ 4,3,3.1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	(*(void(*)())0)();
+//	return 0;
+//}
+//int mian()
+//{
+//	void(*sighnal(int, void(*)(int)))(int);
+//	return 0;
+//}
