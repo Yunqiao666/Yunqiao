61fb2ebab8c0512a483a72625d3254790d623c5a
Yunqiao666 <137733710+Yunqiao666@users.noreply.github.com>
2023-07-09 00:14:58 +0800

    Add files via upload

---
+++ text.4.3.3.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////整形指针数组int* arr[5];
+////函数指针数组
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x-y;
+//}
+//int main()
+//{
+//	int* arr[5]; //整形指针数组
+//	int (*pf1)(int, int) = Add;
+//	int (*pf2)(int, int) = Sub;
+//	int (*pf1arr[10])(int, int) = { Sub,Add };//函数指针数组
+//	//存放同类型函数指针
+//
+//
+//}
